788cf9852767eb6a9dcb08baac207f612d742349
CGLemon <cglemon000@gmail.com>
2021-03-15 16:42:13 +0800

    Boost copyright year.

--- src/Elephant.cc
+++ src/Elephant.cc
@@ -29,7 +29,7 @@
 const static std::string get_license() {
     auto out = std::ostringstream{};
     out << "    ";
-    out << PROGRAM << " " << VERSION << " Copyright (C) 2020  Hung-Zhe Lin"    << std::endl;
+    out << PROGRAM << " " << VERSION << " Copyright (C) 2021  Hung-Zhe Lin"    << std::endl;
     out << "    This program comes with ABSOLUTELY NO WARRANTY."               << std::endl;
     out << "    This is free software, and you are welcome to redistribute it" << std::endl;
     out << "    under certain conditions; see the COPYING file for details."   << std::endl;
